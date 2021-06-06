e4d3f8b34c5d5fe1212b9f356e98dc15b3f40b32
Martin Zvonar <riffmaker@gmail.com>
2021-06-07 00:21:11 +0200

    Chaper 6 in progress

--- exercises/chapter6/test/MySolutions.purs
+++ exercises/chapter6/test/MySolutions.purs
@@ -2,6 +2,10 @@ module Test.MySolutions where
 
 import Prelude
 
+import Data.Newtype (class Newtype, wrap, over2)
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+
 -- Note to reader: Add your solutions to this file
 newtype Point
   = Point
@@ -23,9 +27,26 @@ instance showComplex :: Show Complex where
 
 derive newtype instance eqComplex :: Eq Complex
 
+derive instance newtypeComplex :: Newtype Complex _
+
 instance semiringComplex :: Semiring Complex where
-    zero = Complex {real: 0.0, imaginary: 0.0}
-    add (Complex {real: r1, imaginary: i1}) (Complex {real: r2, imaginary: i2}) = Complex {real: r1 + r2, imaginary: i1 + i2}
-    mul (Complex {real: r1, imaginary: i1}) (Complex {real: r2, imaginary: i2}) = Complex { real: r1 * r2, imaginary: i1 * i2}
-    one = Complex {real: 1.0, imaginary: 1.0}
+    -- zero = wrap {real: 0.0, imaginary: 0.0}
+    zero = wrap zero
+    -- add (Complex {real: r1, imaginary: i1}) (Complex {real: r2, imaginary: i2}) = wrap {real: r1 + r2, imaginary: i1 + i2}
+    add = over2 Complex (+)
+    mul (Complex {real: r1, imaginary: i1}) (Complex {real: r2, imaginary: i2}) = 
+      Complex { real: r1 * r2 - i1 * i2, imaginary: (r1 * i2) + (i1 * r2)}
+    one = wrap {real: 1.0, imaginary: 0.0}
     
+derive newtype instance ringComplex :: Ring Complex
+
+data Shape
+  = Circle Point Number
+  | Rectangle Point Number Number
+  | Line Point Point
+  | Text Point String
+
+derive instance genericShape :: Generic Shape _
+
+instance showShape :: Show Shape where
+  show = genericShow
